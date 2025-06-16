cdd1caa37dd901d311219dfb1c5045980c974d90
at4 <at4@jc4.so-net.ne.jp>
2025-06-16 18:55:02 +0200

    Update scss settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./index.scss";
+import "@/styles/global/index.scss";
 import { Provider } from "@/components/ui/provider";
 import App from "@/App";
 
